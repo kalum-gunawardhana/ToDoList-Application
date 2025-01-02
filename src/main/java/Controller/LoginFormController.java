a714ab07e0b6c9748fb1cf2783f05593dc399402
Kalum Gunawardhana <kguna415@gmail.com>
2025-01-02 17:26:09 +0530

    commit

---
+++ src/main/java/Controller/LoginFormController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    public TextField txtUserName;
+    public TextField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+    }
+}
